c675e7b8f063ae3f6de99e1a9953a77b28107626
mirkomiljon_sharafiddinov <mirkomiljon_sharafiddinov@student.itpu.uz>
2024-03-14 06:44:50 +0500

    add some variables lesson1: task1

--- Lesson 1/Task 1/Program.cs
+++ Lesson 1/Task 1/Program.cs
@@ -1,1 +1,11 @@
 ﻿Console.WriteLine("Hello world!");
+
+int value = 100;
+Console.WriteLine(value);
+
+double a = 7.5;
+char b ='!';
+bool c = true;
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
