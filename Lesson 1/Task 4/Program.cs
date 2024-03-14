fa1f4451fb14c8aa7c211b8c7d7c30f51c487337
mirkomiljon_sharafiddinov <mirkomiljon_sharafiddinov@student.itpu.uz>
2024-03-14 09:41:32 +0500

    add lesson1: task4

---
+++ Lesson 1/Task 4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 6;
+int b = 5;
+int c = 9;
+int d = 7;
+int e = 2;
+
+int a = max;
+
+if (b > max){
+    max = b;
+}
+
+if (c > max){
+    max = c;
+}
+
+if (d > max){
+    max = d;
+}
+
+if (e > max){
+    max = e;
+}
+
+Console.WriteLine(max);
