15d887fb8e00d67346499ec0cda7a1017a3ac419
mirkomiljon_sharafiddinov <mirkomiljon_sharafiddinov@student.itpu.uz>
2024-03-21 21:49:56 +0500

    add Lesson2: Task 4

---
+++ Lesson 2/Task4/Program.cs
@@ -0,0 +1,42 @@
+﻿int [] array = {3,6,80,2,25};
+int i = 0;
+int max = array[0];
+
+while (i < array.Length)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i++;
+}
+
+Console.WriteLine(max);
+
+//
+
+max = array [0];
+for (int j = 0; j < array.Length; j++)
+{
+    if (array[j] > max)
+    {
+        max = array[j];
+    }
+}
+
+Console.WriteLine(max);
+
+//
+
+max = array[0];
+foreach (int e in array)
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+
+Console.WriteLine(max);
+
+
