388f0fa0729325a7522964027a472b7de4ac082d
mirkomiljon_sharafiddinov <mirkomiljon_sharafiddinov@student.itpu.uz>
2024-03-21 21:31:28 +0500

    add Lesson2: Task 3

---
+++ Lesson 2/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿//int n = 10;
+int [] array = {2,4,5,6,23,56,7,9,34,2};
+int i = 0;
+
+while (i < array.Length)
+{
+    if (array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    i++;
+}
