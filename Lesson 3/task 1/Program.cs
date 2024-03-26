7bdbb4c8663030810f9d3699891ec45534b6ea51
mirkomiljon_sharafiddinov <mirkomiljon_sharafiddinov@student.itpu.uz>
2024-03-26 05:28:13 +0500

    add Lesson 3: task 1

---
+++ Lesson 3/task 1/Program.cs
@@ -0,0 +1,10 @@
+﻿//function 
+
+double CalculateFormula(double a, int b, int c, int d)
+{
+    double result = a * b / (c + d);
+    return result;
+}
+
+double result = CalculateFormula(1, 2, 3, 4);
+Console.WriteLine(result);
