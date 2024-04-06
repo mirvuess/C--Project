38711606865049b0ac4ce908da49729a24a3a1cd
mirkomiljon_sharafiddinov <mirkomiljon_sharafiddinov@student.itpu.uz>
2024-04-06 14:37:08 +0500

    added Lesson 4: task 3

---
+++ Lesson 4/task 3/Program.cs
@@ -0,0 +1,36 @@
+﻿string GetLetters(string s)
+{
+    string letters = "";
+    foreach (char e in s)
+    {
+        if (char.IsAsciiLetter(e) == true)
+        {
+            letters += e;
+        }
+    }
+    return letters;
+}
+
+string str = Console.ReadLine();
+string result = GetLetters(str);
+Console.WriteLine(result);
+
+
+//
+
+string GetDigits (string s)
+{
+    string digits = "";
+    foreach (char e in s)
+    {
+        if (char.IsDigit(e))
+        {
+            digits += e;
+        }
+    }
+    return digits;
+}
+
+string str = Console.ReadLine();
+string result = GetDigits(str);
+Console.WriteLine(result);
